2c12cb84df0d9e7a493935d9434c3368a7576a58
AliLlanos <alhidaburitica@gmail.com>
2023-06-14 20:03:52 +0200

    Add functions

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
-package maing
+package main
+
+import "fmt"
+
+func suma(a, b int) int {
+	return a + b
+}
+
+func resta(a, b int) int {
+	return a - b
+}
 
 func main() {
-
+	fmt.Println(suma(6, 3))
+	fmt.Println(resta(8, 8))
 }
